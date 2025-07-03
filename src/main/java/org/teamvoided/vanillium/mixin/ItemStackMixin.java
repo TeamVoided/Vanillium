06d4978b717a80604f653935b81a3d6b0272f581
TheEnderCore <theendercore@gmail.com>
2025-07-03 12:51:55 +0300

    more post use events

--- src/main/java/org/teamvoided/vanillium/mixin/ItemStackMixin.java
+++ src/main/java/org/teamvoided/vanillium/mixin/ItemStackMixin.java
@@ -1,13 +1,18 @@
 package org.teamvoided.vanillium.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.teamvoided.vanillium.events.PostUseItemEvents;
 
 @Mixin(ItemStack.class)
@@ -15,7 +20,30 @@ public class ItemStackMixin {
 
     @ModifyReturnValue(method = "use", at = @At("RETURN"))
     private TypedActionResult<ItemStack> run(TypedActionResult<ItemStack> original, World world, PlayerEntity player, Hand hand) {
-        PostUseItemEvents.EVENT.invoker().interact(original, world, player, hand);
+        PostUseItemEvents.POST_USE.invoker().interact(original, world, player, hand);
         return original;
+    }
+
+    @ModifyReturnValue(method = "useOnBlock", at = @At("RETURN"))
+    private ActionResult run(ActionResult original, ItemUsageContext context) {
+        PostUseItemEvents.POST_USE_ON_BLOCK.invoker().interact(original, context);
+        return original;
+    }
+
+    @ModifyReturnValue(method = "useOnEntity", at = @At("RETURN"))
+    private ActionResult run(ActionResult original, PlayerEntity user, LivingEntity entity, Hand hand) {
+        PostUseItemEvents.POST_USE_ON_ENTITY.invoker().interact(original, user, entity, hand);
+        return original;
+    }
+
+    @ModifyReturnValue(method = "finishUsing", at = @At("RETURN"))
+    private ItemStack run(ItemStack original, World world, LivingEntity user) {
+        PostUseItemEvents.POST_USING.invoker().interact(original, (ItemStack) (Object) this, world, user);
+        return original;
+    }
+
+    @Inject(method = "onStoppedUsing", at = @At("TAIL"))
+    private void run(World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci) {
+        PostUseItemEvents.POST_STOP_USING.invoker().interact((ItemStack) (Object) this, world, user, remainingUseTicks);
     }
 }
--- src/main/kotlin/org/teamvoided/vanillium/events/PostUseItemEvents.kt
+++ src/main/kotlin/org/teamvoided/vanillium/events/PostUseItemEvents.kt
@@ -2,22 +2,72 @@ package org.teamvoided.vanillium.events
 
 import net.fabricmc.fabric.api.event.Event
 import net.fabricmc.fabric.api.event.EventFactory.createArrayBacked
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
+import net.minecraft.item.ItemUsageContext
+import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
 
 object PostUseItemEvents {
-    fun interface PostUseItemCallback {
-        fun interact(original: TypedActionResult<ItemStack>, world: World, player: PlayerEntity, hand: Hand)
+    @JvmField
+    val POST_USE: Event<PostUseItemCallback> = createArrayBacked(PostUseItemCallback::class.java) { listeners ->
+        PostUseItemCallback { returned, world, player, hand ->
+            listeners.forEach { it.interact(returned, world, player, hand) }
+        }
     }
 
     @JvmField
-    val EVENT: Event<PostUseItemCallback> = createArrayBacked(PostUseItemCallback::class.java) { listeners ->
-        PostUseItemCallback { original: TypedActionResult<ItemStack>, world: World, player: PlayerEntity, hand: Hand ->
-            listeners.forEach { it.interact(original, world, player, hand) }
+    val POST_USE_ON_BLOCK: Event<PostUseOnBlockItemCallback> =
+        createArrayBacked(PostUseOnBlockItemCallback::class.java) { listeners ->
+            PostUseOnBlockItemCallback { returned, context ->
+                listeners.forEach { it.interact(returned, context) }
+            }
         }
+
+    @JvmField
+    val POST_USE_ON_ENTITY: Event<PostUseOnEntityItemCallback> =
+        createArrayBacked(PostUseOnEntityItemCallback::class.java) { listeners ->
+            PostUseOnEntityItemCallback { returned, player, entity, hand ->
+                listeners.forEach { it.interact(returned, player, entity, hand) }
+            }
+        }
+
+    @JvmField
+    val POST_USING: Event<PostUsingItemCallback> = createArrayBacked(PostUsingItemCallback::class.java) { listeners ->
+        PostUsingItemCallback { returned, usedSTack, world, player ->
+            listeners.forEach { it.interact(returned, usedSTack, world, player) }
+        }
+    }
+
+    @JvmField
+    val POST_STOP_USING: Event<PostStopUsingItemCallback> =
+        createArrayBacked(PostStopUsingItemCallback::class.java) { listeners ->
+            PostStopUsingItemCallback { stack, world, user, remainingUseTicks ->
+                listeners.forEach { it.interact(stack, world, user, remainingUseTicks) }
+            }
+        }
+
+    fun interface PostUseItemCallback {
+        fun interact(returned: TypedActionResult<ItemStack>, world: World, player: PlayerEntity, hand: Hand)
+    }
+
+    fun interface PostUseOnBlockItemCallback {
+        fun interact(returned: ActionResult, context: ItemUsageContext)
+    }
+
+    fun interface PostUseOnEntityItemCallback {
+        fun interact(returned: ActionResult, world: PlayerEntity, player: LivingEntity, hand: Hand)
+    }
+
+    fun interface PostUsingItemCallback {
+        fun interact(returned: ItemStack, usedStack: ItemStack, world: World, player: LivingEntity)
+    }
+
+    fun interface PostStopUsingItemCallback {
+        fun interact(usedStack: ItemStack, world: World, player: LivingEntity, remainingUseTicks: Int)
     }
 }
 
