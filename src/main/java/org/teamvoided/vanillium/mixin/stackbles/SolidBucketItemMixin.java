883921ee7d42a623e078abcdb9c3ed673c561cb1
TheEnderCore <theendercore@gmail.com>
2026-07-20 22:21:44 +0300

    fxied bug with powdered snow buckets

---
+++ src/main/java/org/teamvoided/vanillium/mixin/stackbles/SolidBucketItemMixin.java
@@ -0,0 +1,25 @@
+package org.teamvoided.vanillium.mixin.stackbles;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SolidBucketItem;
+import net.minecraft.world.item.context.UseOnContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(SolidBucketItem.class)
+public class SolidBucketItemMixin {
+
+    @WrapOperation(method = "useOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setItemInHand(Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;)V"))
+    void fixItemDeletion(Player player, InteractionHand hand, ItemStack itemStack, Operation<Void> original, UseOnContext ctx) {
+        var stack = ctx.getItemInHand();
+        if (itemStack != null && !itemStack.isEmpty()) {
+            if (!player.getInventory().add(itemStack)) {
+                player.drop(itemStack, false);
+            }
+        }
+    }
+}
